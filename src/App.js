c1dff1a0039be23d037aae4fda836295d26461a1
Bubuka Sharif <sharifbubuka256@gmail.com>
2021-10-28 12:43:42 +0300

    gh-pages branch added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 
-import {
-  Renderer,
-} from './components/reusables';
+// import {
+//   Renderer,
+// } from './components/reusables';
 
 import {
   HomePage,
